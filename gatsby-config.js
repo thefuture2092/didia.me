c665178a6b10c931f1d598f05291c0234bd2f8f4
Aristote Diasonama <aristotediasonama@yahoo.fr>
2019-06-24 23:34:16 -0400

    Fix sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -159,7 +159,7 @@ module.exports = {
         output: '/sitemap.xml',
         serialize: ({site, allSitePage}) =>
           allSitePage.edges.map(edge => ({
-            url: `${site.siteMetadata.url + edge.node.path}/`,
+            url: `${site.siteMetadata.siteUrl + edge.node.path}/`,
             changefreq: 'daily',
             priority: 0.7
           }))
