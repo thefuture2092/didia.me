3d4e125fe889809aba30543189d7f4721598d215
Aristote Diasonama <aristotediasonama@yahoo.fr>
2019-09-23 01:04:45 -0400

    Run prettier

--- src/components/BookFeed/BookFeed.js
+++ src/components/BookFeed/BookFeed.js
@@ -12,36 +12,53 @@ const BookActionButtons = ({edge}) => {
   return (
     <CardActions>
       <CardActionButtons>
-        {edge.node.html ? <Link className={'mdc-button mdc-card__action mdc-card__action--button'} to={edge.node.fields.slug}>
-          Lire mon résumé
-        </Link> : <></>}
-        {edge.node.frontmatter.amazonLink ? <a className={'mdc-button mdc-card__action mdc-card__action--button'} href={edge.node.frontmatter.amazonLink} target="_blank" rel="noopener noreferrer">
-          Acheter
-        </a> : <></>}
+        {edge.node.html ? (
+          <Link className={'mdc-button mdc-card__action mdc-card__action--button'} to={edge.node.fields.slug}>
+            Lire mon résumé
+          </Link>
+        ) : (
+          <></>
+        )}
+        {edge.node.frontmatter.amazonLink ? (
+          <a
+            className={'mdc-button mdc-card__action mdc-card__action--button'}
+            href={edge.node.frontmatter.amazonLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Acheter
+          </a>
+        ) : (
+          <></>
+        )}
       </CardActionButtons>
     </CardActions>
   );
-}
+};
 
-const ReadingTime = ({edge}) => (
-  edge.node.frontmatter.readingEnd ?
-    <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.readingStart).format('MMMM D, YYYY')}>
-      {`Lu du ${moment(edge.node.frontmatter.readingStart).format('D MMMM YYYY')} au ${moment(edge.node.frontmatter.readingEnd).format('D MMMM YYYY')}`}
+const ReadingTime = ({edge}) =>
+  edge.node.frontmatter.readingEnd ? (
+    <time
+      className={styles['feed__item-meta-time']}
+      dateTime={moment(edge.node.frontmatter.readingStart).format('MMMM D, YYYY')}
+    >
+      {`Lu du ${moment(edge.node.frontmatter.readingStart).format('D MMMM YYYY')} au ${moment(
+        edge.node.frontmatter.readingEnd
+      ).format('D MMMM YYYY')}`}
     </time>
-   : <time
+  ) : (
+    <time
       className={styles['feed__item-meta-time']}
       dateTime={moment(edge.node.frontmatter.readingStart).format('MMMM D, YYYY')}
     >
       {`En lecture depuis le ${moment(edge.node.frontmatter.readingStart).format('D MMMM YYYY')}`}
     </time>
-);
+  );
 
 const CurrentBook = ({edge}) => (
   <Card className={classnames(styles['feed__item'], 'mdc-card')}>
     <div className={styles['feed__item-body']}>
-      <Headline5 className={styles['feed__item-title']}>
-        {edge.node.frontmatter.title}
-      </Headline5>
+      <Headline5 className={styles['feed__item-title']}>{edge.node.frontmatter.title}</Headline5>
       <Subtitle2 className={classnames(styles['feed__item-subtitle'], styles['feed__item-subtitle--author'])}>
         Par {edge.node.frontmatter.author}
       </Subtitle2>
@@ -49,15 +66,13 @@ const CurrentBook = ({edge}) => (
     <CardPrimaryContent className={styles['feed__item-content']}>
       <CardMedia wide imageUrl={edge.node.frontmatter.thumbnail.childImageSharp.fluid.src} />
       <div className={styles['feed__item-body']}>
-        <Body2 className={styles['feed__item-description']}>
-          {edge.node.frontmatter.description}
-        </Body2>
+        <Body2 className={styles['feed__item-description']}>{edge.node.frontmatter.description}</Body2>
         <Subtitle2 className={styles['feed__item-subtitle']}>
           <ReadingTime edge={edge} />
         </Subtitle2>
       </div>
     </CardPrimaryContent>
-    <BookActionButtons edge={edge}/>
+    <BookActionButtons edge={edge} />
   </Card>
 );
 
@@ -87,7 +102,9 @@ const Book = ({edge}) => (
 const BookFeed = ({edges}) => [
   <CurrentBook edge={edges[0]} />,
   <div className={styles['feed']} key="feed">
-    {edges.slice(1).map(edge => <Book edge={edge} key={edge.node.fields.slug} />)}
+    {edges.slice(1).map(edge => (
+      <Book edge={edge} key={edge.node.fields.slug} />
+    ))}
   </div>
 ];
 
